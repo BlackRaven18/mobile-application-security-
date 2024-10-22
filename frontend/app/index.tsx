293c245c8bc8dba79dd1f3718533962fe25a4866
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-22 13:03:31 +0200

    Added login and password fields validation

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,12 @@
 import { Button, IconButton, Stack, Text, TextInput } from "@react-native-material/core";
 import React, { useState } from "react";
-import { StyleSheet, View} from "react-native";
+import { StyleSheet, View } from "react-native";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import axios from "axios";
 
 export default function Index() {
 
+  const forbiddenChars = ["/", "\\", ":", "*", "?", "\"", "<", ">", "|"];
   const backendUrl = "http://192.168.4.164:3000";
   // const backendUrl = "http://localhost:3000";
 
@@ -13,25 +14,54 @@ export default function Index() {
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
 
+  const onLoginFieldChange = (value: string) => {
+    if (forbiddenChars.some((char) => value.includes(char))) {
+      setMessage("Forbidden characters in login field");
+      return;
+    }
+
+    setLogin(value);
+  }
+
+  const onPasswordFieldChange = (value: string) => {
+    if (forbiddenChars.some((char) => value.includes(char))) {
+      setMessage("Forbidden characters in password field");
+      return;
+    }
+
+    setPassword(value);
+  }
+
+  const validateLoginData = (login: string, password: string) => {
+    if (login === "" || password === "") {
+      setMessage("Login and password cannot be empty");
+      return false;
+    }
+    return true;
+  }
+
   const sendLogin = (login: string, password: string) => {
+    if (!validateLoginData(login, password)) {
+      return;
+    }
     console.log("Sended login request");
     axios.post(backendUrl + "/login", { login: login, password: password }).then((response) => {
       console.log(response.data);
       setMessage(response.data + ": " + login);
     })
-    .catch((error) => {
-      console.log(error);
-      setMessage(error);
-    })
+      .catch((error) => {
+        console.log(error);
+        setMessage(error);
+      })
   }
 
   return (
-    <Stack spacing={2} style={{ margin: 16}}>
+    <Stack spacing={2} style={{ margin: 16 }}>
       <TextInput
         label="Login"
         leading={props => <Icon name="account" {...props} />}
         value={login}
-        onChangeText={setLogin}
+        onChangeText={onLoginFieldChange}
       />
       <TextInput
         label="Password"
@@ -40,11 +70,11 @@ export default function Index() {
           <IconButton icon={props => <Icon name="eye" {...props} />} {...props} />
         )}
         value={password}
-        onChangeText={setPassword}
+        onChangeText={onPasswordFieldChange}
       />
 
-        <Button title="Login" onPress={() => sendLogin(login, password)} />
-        <Text>{message}</Text>
+      <Button title="Login" onPress={() => sendLogin(login, password)} />
+      <Text>{message}</Text>
     </Stack>
 
   );
