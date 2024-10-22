3bed21a4f88567b2f8ed4fb2756cdcf7b867296c
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-10-22 19:48:33 +0200

    Secured backend from SLQ Injections attacks

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const sqlite3 = require('sqlite3').verbose();
 var cors = require('cors')
+var AuthUtils = require('./AuthUtils');
 
 const db = new sqlite3.Database('./testdb.db', (err) => {
   if (err) {
@@ -12,20 +13,27 @@ const db = new sqlite3.Database('./testdb.db', (err) => {
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/login', cors(), (req, res) => {
-  let query = `SELECT * FROM users WHERE login = '${req.body.login}' AND password = '${req.body.password}'`
+app.post('/login', (req, res) => {
 
-  db.get(query, (err, row) => {
+  const authUtils = new AuthUtils();
+  if (!authUtils.validateString(req.body.login) || !authUtils.validateString(req.body.password)) {
+    res.send("Invalid data")
+    return
+  }
+
+  const stmt = db.prepare("SELECT * FROM users WHERE login = ? AND password = ?");
+  
+  stmt.get([req.body.login, req.body.password], (err, row) => {
     if (err) {
       console.error(err.message);
     }
-
     if (row) {
       res.send("User found")
     } else {
